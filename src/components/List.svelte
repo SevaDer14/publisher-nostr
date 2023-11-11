1bdaf4349097d97d752682b70032e7c616d537e2
Seva Deriushkin <vsevolod.deriushkin@gmail.com>
2023-11-11 16:01:06 +0100

    adds List component

---
+++ src/components/List.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import ListItem from './ListItem.svelte';
+
+	export let items: {
+		type: string;
+		author: string;
+		title: string;
+		tags: string[];
+		price: number;
+	}[] = [];
+</script>
+
+<ul data-cy="list" class="h-full w-full rounded-lg border border-zinc-200 bg-white overflow-y-auto">
+	{#each items as item, index}
+		<ListItem {...item} />
+		{#if index < items.length - 1}
+			<div class="h-[1px] w-full bg-zinc-200" />
+		{/if}
+	{/each}
+</ul>
+
+<style lang="postcss">
+</style>
